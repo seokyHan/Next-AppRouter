ec2ff2c074401b5c997e517c0b2ce68300c9a763
seoky <sseok@springKingui-MacBookPro.local>
2025-05-22 01:12:16 +0900

    feat: request memoization test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,29 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if(!response.ok) {
+    return <div>오류가 발생했습니다!</div>
+  }
+
+  const books : BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @sseok</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  )
+}
+
+// Next에서는 중복된 요청에 대해서 리퀘스트 메모이제이션을 통해 자동으로 캐시해줌.
+// 그렇게 되면 모든 컴포넌트에서 한번의 요청으로 사용
 
 export default function RootLayout({
   children,
@@ -15,7 +38,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
