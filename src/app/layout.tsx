14b36b50cd0e972f9c00e86d7c474818bf7fb906
seoky <sseok@springKingui-MacBookPro.local>
2025-05-23 01:38:57 +0900

    feat: error handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,23 +4,23 @@ import style from "./layout.module.css";
 import { BookData } from "@/types";
 
 async function Footer() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
-    {cache: "force-cache"}
-  );
-  if(!response.ok) {
-    return <div>오류가 발생했습니다!</div>
-  }
+  // const response = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+  //   {cache: "force-cache"}
+  // );
+  // if(!response.ok) {
+  //   return <div>오류가 발생했습니다!</div>
+  // }
 
-  const books : BookData[] = await response.json();
-  const bookCount = books.length;
+  // const books : BookData[] = await response.json();
+  // const bookCount = books.length;
 
-  return (
-    <footer>
-      <div>제작 @sseok</div>
-      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
-    </footer>
-  )
+  // return (
+  //   <footer>
+  //     <div>제작 @sseok</div>
+  //     <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+  //   </footer>
+  // )
 }
 
 export default function RootLayout({
