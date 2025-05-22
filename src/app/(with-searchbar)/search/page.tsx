be20f260001ca83ebe2879b7ba90fc26c7ab3368
seoky <sseok@springKingui-MacBookPro.local>
2025-05-23 01:14:19 +0900

    feat: skeleton ui 적용

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -2,6 +2,7 @@ import { delay } from "@/app/util/delay";
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 import { Suspense } from "react";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 
 async function SearchResult({q}: { q: string }) {
   await delay(1500)
@@ -30,9 +31,14 @@ export default function Page({
   searchParams: {q?: string };
 }) {
   return (
-    <Suspense key={searchParams.q || ""} fallback={<div>Loading...</div>}>
+
+    <Suspense 
+      key={searchParams.q || ""} 
+      fallback={<BookListSkeleton count={3} />}
+      >        
       <SearchResult q={searchParams.q || ""} />
     </Suspense>
+    
   )
   
 }
